c0018a4efcace482b2f169bdb9557d2bd9c77c47
nicolasmichielon <nicolasmichielon@gmail.com>
2025-12-08 19:33:39 -0300

    refactor: remove commented-out code and TODOs from renderer functions

--- src/core/renderer.cpp
+++ src/core/renderer.cpp
@@ -19,17 +19,6 @@ void desenharCirculo(SDL_Renderer* renderer, int cx, int cy, int raio, bool pree
     }
 }
 
-void desenharCenario(SDL_Renderer* renderer) {
-    // TODO: Desenhar fundo, chão, céu
-    // Exemplo:
-    // SDL_SetRenderDrawColor(renderer, 10, 10, 30, 255); // Céu escuro
-    // SDL_RenderClear(renderer);
-
-    // Linha do chão
-    // SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
-    // SDL_RenderDrawLine(renderer, 0, ALTURA_TELA - 50, LARGURA_TELA, ALTURA_TELA - 50);
-}
-
 void desenharHUD(SDL_Renderer* renderer, EstadoJogo* estado) {
     if (!estado) return;
 
@@ -62,24 +51,6 @@ void desenharHUD(SDL_Renderer* renderer, EstadoJogo* estado) {
             SDL_RenderDrawPoint(renderer, px, py);
         }
     }
-
-    // TODO: Adicionar texto mostrando naves abatidas, escaparam, etc
-}
-
-void renderizarTudo(SDL_Renderer* renderer, EstadoJogo* estado) {
-    // TODO: Desenhar tudo na ordem correta
-    // 1. Cenário (fundo)
-    // 2. Naves
-    // 3. Foguetes
-    // 4. Bateria
-    // 5. HUD
-    // 6. SDL_RenderPresent(renderer);
-}
-
-void desenharTexto(SDL_Renderer* renderer, const char* texto, int x, int y) {
-    // TODO (opcional): Desenhar texto na tela
-    // Por enquanto pode usar retângulos para representar números
-    // Ou integrar SDL_ttf para fontes reais
 }
 
 // Desenha um dígito usando segmentos (estilo display 7 segmentos)
